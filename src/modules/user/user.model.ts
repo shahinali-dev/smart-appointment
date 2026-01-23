ecd9c45bc7ae00f595493ea569aa02b6e55e0bbd
shahinli-dev <shahinali.dev@gmail.com>
2026-01-23 11:07:11 +0600

    refactor: remove pre middleware from user schema

--- src/modules/user/user.model.ts
+++ src/modules/user/user.model.ts
@@ -1,6 +1,4 @@
-import bcrypt from "bcrypt";
 import { model, Schema } from "mongoose";
-import config from "../../config";
 import { Role } from "./user.enum";
 import { IUser } from "./user.interface";
 
@@ -22,20 +20,8 @@ const userSchema = new Schema<IUser>(
   },
   {
     timestamps: true,
-  }
+  },
 );
-
-// Hash the password before saving if it's a local user
-userSchema.pre("save", async function (next) {
-  if (this.password && this.isModified("password")) {
-    const hashedPassword = await bcrypt.hash(
-      this.password,
-      Number(config.SALT_ROUNDS)
-    );
-    this.password = hashedPassword;
-  }
-  next();
-});
 
 const UserModel = model<IUser>("User", userSchema);
 
