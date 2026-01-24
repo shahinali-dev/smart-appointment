0a6ee8620ce152092462e56b6cdea4dffdd9d365
shahinli-dev <shahinali.dev@gmail.com>
2026-01-24 23:25:20 +0600

    feat: Implement service validation schema

---
+++ src/modules/service/service.validation.ts
@@ -0,0 +1,24 @@
+import { z } from "zod";
+import { ServiceDuration, ServiceType } from "./service.enum";
+
+const createServiceValidationSchema = z.object({
+  serviceName: z.string().min(1, "Service name is required").trim(),
+  duration: z.nativeEnum(ServiceDuration, {
+    errorMap: () => ({
+      message: "Invalid duration. Must be 15, 30, or 60 minutes",
+    }),
+  }),
+  requiredStaffType: z.nativeEnum(ServiceType, {
+    errorMap: () => ({ message: "Invalid staff type" }),
+  }),
+});
+
+const updateServiceValidationSchema = createServiceValidationSchema.partial();
+
+export type ICreateService = z.infer<typeof createServiceValidationSchema>;
+export type IUpdateService = z.infer<typeof updateServiceValidationSchema>;
+
+export const serviceValidation = {
+  createServiceValidationSchema,
+  updateServiceValidationSchema,
+};
